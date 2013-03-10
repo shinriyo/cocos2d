08dedc59a61772c1a0b4250f7ecadf4279b500b0
shinriyo <shinriyo@gmail.com>
2013-03-10 14:07:20 +0900

    Initial Commit

---
+++ test/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  testAppController.h
+//  test
+//
+//  Created by sugita on 13/03/10.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
